c82dab12b18794df17e85b72098f0070ccd1c940
spl0i7 <ketansingh.sh@gmail.com>
2016-01-16 14:31:55 +0530

    Initial release
    python file splitter

---
+++ fileSpiltter.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import os
+import sys
+import shutil
+def fileSplitter(file,chunk,tmpDirname):
+    fileSize=os.path.getsize(file)
+    chunkSize=fileSize/chunk
+    print("File Size : "+fileSize+ " bytes")
+    print("Chunk Size : "+chunkSize+ " bytes")
+    print("Parts : "+ chunk)
+    print("")
+    pointer=chunkSize
+    tmpfileName='part'
+    tmpFilecount=0
+    configureDir(tmpDirname)
+    fr=open(file,'rb')
+    while (True):
+        fr.seek(pointer-chunkSize)
+        buf=fr.read(chunkSize)
+        if(buf==''):
+            break
+        fw=open(tmpDirname+'/'+tmpfileName+str(tmpFilecount),'wb')
+        fw.write(buf)
+        fw.close()
+        pointer+=chunkSize
+        tmpFilecount+=1
+
+def cleanupTmp(tmpDirname):
+    shutil.rmtree(tmpDirname)
+
+def fileMerger(tmpDirname,finalFileName,parts):
+    tmpfileName='part'
+    tmpFilecount=0
+    while(tmpFilecount <= parts):
+        fr=open(tmpDirname+'/'+tmpfileName+str(tmpFilecount),'rb')
+        buf=fr.read(os.path.getsize(tmpDirname+'/'+tmpfileName+str(tmpFilecount)))
+        fw=open(finalFileName,'ab')
+        fw.write(buf)
+        fw.close()
+        fr.close()
+        tmpFilecount+=1
+    cleanupTmp(tmpDirname)
+
+def printman():
+    print("[+] Simple Python File Splitter")
+    print("[+] by spl0i7 https://www.ketansingh.me")
+    print("[+] To split usage : "+sys.argv[0]+" s <file> <parts> <path to store parts>")
+    print("[+] To merge back : "+sys.argv[0]+" m <new filename> <parts> <path of parts>")
+
+def configureDir(dirName):
+    try:
+        if(os.path.exists(dirName)):
+            shutil.rmtree(dirName)
+        os.mkdir(dirName)
+    except OSError:
+        print("Error happened in creating tmp dir")
+
+if len(sys.argv )==5:
+    if(sys.argv[1]=="s"):
+        fileSplitter(sys.argv[2],int(sys.argv[3]),sys.argv[4])
+    elif(sys.argv[1]=="m"):
+        fileMerger(sys.argv[4],sys.argv[2],int(sys.argv[3]))
+    else :
+        printman()
+else:
+    printman()
